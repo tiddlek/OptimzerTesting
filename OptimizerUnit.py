fdeb24d4d0d48f58b0273f973b2b3c60de70ac63
Keenan Tiddle <tiddlek@whitman.edu>
2025-02-01 21:21:55 -0800

    Cutting Down Code to only GP Model

--- OptimizerUnit.py
+++ OptimizerUnit.py
@@ -46,17 +46,7 @@ def prepPlot():
     plt.title("Matern Kernel", fontsize = 16, pad = 20, loc="center")
 
 def f(x):
-    print(f"X: {np.array(x[0][0])}")
-    X_denormalized = denormalize(x[0][0],0,0.002)
-    print(f"Concentration {X_denormalized}")
-    y = gp_model.predict(x) 
-    print(f"Prediction: {y[0][0][0]}")
-    Y_denormalized = denormalize(y[0][0][0],300,900)
-    print(f"Denormalized Prediction: {Y_denormalized}")
-    result = abs(target - Y_denormalized)
-    print(f"Target - Prediction: {result}")
-    print("===")
-    return result
+    return None
 
 def normalize(x, min_val, max_val):
     return (x - min_val) / (max_val - min_val)
@@ -72,8 +62,6 @@ Y_normalized = normalize(Y, 300, 900)
 domain = [{'name': 'x', 'type': 'continuous', 'domain': (0, 1)}]
 space = GPyOpt.core.task.space.Design_space(domain)
 
-Exponential = GPy.kern.Exponential(1)
-Radial = GPy.kern.RBF(1)
 Matern32 = GPy.kern.Matern32(1, lengthscale=1, variance=1)
 
 gp_model = GPyOpt.models.GPModel(kernel=Matern32, noise_var=1e-4, optimize_restarts=0, verbose=False)
@@ -81,22 +69,14 @@ gp_model.updateModel(X_normalized, Y_normalized, None, None)
 acq_optimizer = GPyOpt.optimization.AcquisitionOptimizer(space, optimizer="lbfgs")
 
 EI = GPyOpt.acquisitions.AcquisitionEI(gp_model,space,acq_optimizer)
-MPI = GPyOpt.acquisitions.AcquisitionMPI(gp_model,space,acq_optimizer,jitter=0.01)
-LCB_Explore = GPyOpt.acquisitions.AcquisitionLCB(gp_model,space,acq_optimizer,exploration_weight=15)
-LCB_Exploit = GPyOpt.acquisitions.AcquisitionLCB(gp_model,space,acq_optimizer,exploration_weight=5)
 
 objective = GPyOpt.core.task.objective.SingleObjective(f)
-evaluator = GPyOpt.core.evaluators.Sequential(MPI)
-model = GPyOpt.methods.ModularBayesianOptimization(objective=objective,model=gp_model,space=space,acquisition=MPI,evaluator=evaluator,X_init=X_normalized,Y_init=Y_normalized, normalize_Y=False)
+evaluator = GPyOpt.core.evaluators.Sequential(EI)
+model = GPyOpt.methods.ModularBayesianOptimization(objective=objective,model=gp_model,space=space,acquisition=EI,evaluator=evaluator,X_init=X_normalized,Y_init=Y_normalized, normalize_Y=False)
 
 predictions, stdev = getPredictions()
 predictions = denormalize(np.array(predictions).flatten(),300, 900)
 stdev = np.array(stdev).flatten()*(600)
-
-model.run_optimization(5)
-
-next = denormalize(model.suggest_next_locations()[0][0],0,0.002)
-print(next)
 
 linespace = np.linspace(0,0.002,200).tolist()
 
@@ -108,8 +88,8 @@ plt.fill_between(
     predictions - stdev,
     alpha = 0.3
 )
-plt.scatter(next,550)
+
 plt.plot(linespace, predictions)
-plt.scatter(X,Y)
+plt.scatter(X,Y, c="#2ca02c",s=25)
 plt.tight_layout()
 plt.show()
